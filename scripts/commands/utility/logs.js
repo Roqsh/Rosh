f4a4c9f19974e061989f4483f692910cfb78a205
Rosh <linus.kazaltzis@gmail.com>
2024-05-27 18:06:41 +0200

    (*) Improved logs command
    - playerTellraw is no longer used and sendMessage is an easier implementation

--- scripts/commands/utility/logs.js
+++ scripts/commands/utility/logs.js
@@ -1,18 +1,21 @@
 import data from "../../data/data";
-import { playerTellraw } from "../../utils/gameUtil";
+
 /**
  * @name logs
  * @param {object} message - Message object
  */
+
 export function logs(message) {
-    // validate that required params are defined
+    // Validate the input
     if(typeof message !== "object") throw TypeError(`message is type of ${typeof message}. Expected "object".`);
 
     const player = message.sender;
     
-    let logs = data.recentLogs;          
-    playerTellraw(player, "§r§8- §uRosh Logs §8-")
+    let logs = data.recentLogs;   
+
+    player.sendMessage("§r§8- §uRosh Logs §8-");  
+
     for (let i = 0; i < logs.length; i++) {
-        playerTellraw(player, logs[i]);
+        player.sendMessage(logs[i]);
     }
 }
