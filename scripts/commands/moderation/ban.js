bc3b03e7e6138e2c27c282a93659e6d602ec8ba3
Rosh <linus.kazaltzis@gmail.com>
2024-06-14 17:02:10 +0200

    (#) Fixed !ban not working

--- scripts/commands/moderation/ban.js
+++ scripts/commands/moderation/ban.js
@@ -1,6 +1,7 @@
 import * as Minecraft from "@minecraft/server";
 import { parseTime } from "../../util.js";
 import data from "../../data/data.js";
+import config from "../../data/config.js";
 
 const world = Minecraft.world;
 
@@ -10,14 +11,15 @@ export function ban(message, args) {
     if(typeof args !== "object") throw TypeError(`args is type of ${typeof args}. Expected "object".`);
 
     const player = message.sender;
+    let themecolor = config.themecolor;
 
-    if(!args.length) return player.sendMessage(`§r§uRosh §j> §cYou need to provide who to ban.`);
+    if(!args.length) return player.sendMessage(`§r${themecolor}Rosh §j> §cYou need to provide who to ban.`);
 
     const time = args[1] ? parseTime(args[1]) : undefined;
 
     if(!time) args.splice(1, 1);
 
-    const reason = args.slice(1).join(" ").replace(/"|\\/g, "") || "No reason specified";
+    const reason = args.slice(1).join(" ").replace(/"|\\/g, "");
     
     let member;
 
@@ -26,20 +28,20 @@ export function ban(message, args) {
         break;
     }
 
-    if(!member) return player.sendMessage(`§r§uRosh §j> §cCouldn't find that player.`);
+    if(!member) return player.sendMessage(`§r${themecolor}Rosh §j> §cCouldn't find that player.`);
 
-    if(member.id === player.id) return player.sendMessage(`§r§uRosh §j> §cYou cannot ban yourself.`);
+    if(member.id === player.id) return player.sendMessage(`§r${themecolor}Rosh §j> §cYou cannot ban yourself.`);
    
-    if(member.hasTag("op")) return player.sendMessage(`§r§uRosh §j> §cYou cannot ban other staff members.`);
+    if(member.hasTag("op")) return player.sendMessage(`§r${themecolor}Rosh §j> §cYou cannot ban other staff members.`);
   
     member.getTags().forEach(t => {
-        if(t.includes("reason:") || t.includes("time:")) member.removeTag(t);
+        if(t.includes("Reason:") || t.includes("Length:")) member.removeTag(t);
     });
 
-    member.addTag(`reason:${reason}`);
-    if(typeof time === "number") member.addTag(`time:${Date.now() + time}`);
+    member.addTag(`Reason:${reason}`);
+    if(typeof time === "number") member.addTag(`Length:${Date.now() + time}`);
     member.addTag("isBanned");
 
-    player.runCommandAsync(`tellraw @a[tag=op] {"rawtext":[{"text":"§r§uRosh §j> §8${player.nameTag} §chas banned §8${member.nameTag} §cfor §8${reason}"}]}`);
+    player.runCommandAsync(`tellraw @a[tag=op] {"rawtext":[{"text":"§r${themecolor}Rosh §j> §8${player.nameTag} §chas banned §8${member.nameTag} §cfor §8${reason}"}]}`);
     data.recentLogs.push(`§8${member.nameTag} §chas been banned by §8${player.name}§c!`);
 }
