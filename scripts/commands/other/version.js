8287ef22813b8b9b7d1dbf468d7081504927baa1
Rosh <linus.kazaltzis@gmail.com>
2024-06-15 13:09:33 +0200

    (*) Set version to v1.23 [Dev]

--- scripts/commands/other/version.js
+++ scripts/commands/other/version.js
@@ -2,6 +2,6 @@ export function version(message) {
 
     const player = message.sender; 
 
-    player.runCommandAsync(`tellraw @s {"rawtext":[{"text":"§r§uRosh §j> §aRosh is currently at §81.22§a!"}]}`);
+    player.runCommandAsync(`tellraw @s {"rawtext":[{"text":"§r§uRosh §j> §aRosh is currently at §81.23 [Dev] §a!"}]}`);
 
 }
